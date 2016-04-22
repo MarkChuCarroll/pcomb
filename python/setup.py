5b175db33ddd9ba3c232201cdf8e3df7e10e15d6
Spencer Bliven <sbliven@ucsd.edu>
2016-04-22 20:34:57 +0200

    Adding setuptools build script to python module

---
+++ python/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pcomb',
+      version='0.1',
+      description='Very Basic Combinator Parser',
+      url='https://github.com/MarkChuCarroll/pcomb',
+      author='Mark Chu-Carroll',
+      author_email='mchucarroll@twitter.com',
+      license='Apache 2.0',
+      packages=['pcomb'],
+      zip_safe=False)
+
